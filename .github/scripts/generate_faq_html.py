93dde7363dcabe0ad71a2048b4d2357551456608
NOT_A_ROBOT <not_a_normal_robot@protonmail.com>
2024-01-07 08:38:43 +0700

    Test CI

---
+++ .github/scripts/generate_faq_html.py
@@ -0,0 +1,34 @@
+import json
+
+# Constants
+faq_json_location = "data/faq.json"
+faq_html_template_location = "data/faq.html.template"
+template_insertion_location = "<INSERT_QUESTIONS>"
+
+def faq_html(entry):
+    return f"""
+    <div class="question">
+        <button class="question-header">
+            <div class="question-icon-container">
+                <svg class="question-icon" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M7 14l5-5 5 5z"></path>
+                </svg>
+            </div>
+            <h2 class="question-title">{entry['question']}</h2>
+        </button>
+        <div class="answer">
+            {entry['answerHTML']}
+            <div class="answer-source">— {entry['author']}</div>
+        </div>
+    </div>
+    """
+
+faq = json.load(open(faq_json_location, "r"))
+html_template = open(faq_html_template_location, "r").read()
+text_to_insert = ""
+for entry in faq:
+    text_to_insert += faq_html(entry)
+
+html_template = html_template.replace(template_insertion_location, text_to_insert)
+
+open("data/faq.html", "w").write(html_template)
