593382a5e43b620ea50744fdb6c7f0c6125737aa
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2024-01-28 18:44:54 +0700

    Update script

--- .github/scripts/generate_faq_html.py
+++ .github/scripts/generate_faq_html.py
@@ -1,9 +1,15 @@
-import json
+try: # pip install json-with-comments
+    import jsonc; json=jsonc
+    use_jsonc=True
+except:
+    import json
+    use_jsonc=False
 
 # Constants
-faq_json_location = "data/faq.json"
-faq_html_template_location = "data/faq-template.html"
-faq_html_output_location = "faq.html"
+faq_json_location  = "data/faq.json"
+faq_jsonc_location = "data/faq.jsonc"
+faq_html_template_location  = "data/faq-template.html"
+faq_html_output_location    = "faq.html"
 template_insertion_location = "<INSERT_QUESTIONS>"
 
 def faq_html(entry):
@@ -17,19 +23,19 @@ def faq_html(entry):
             </div>
             <h2 class="question-title">{entry['question']}</h2>
         </button>
-        <div class="answer">
+        <div class="answer" contenteditable="true">
             {entry['answerHTML']}
             <div class="answer-source">— {entry['author']}</div>
         </div>
     </div>
     """
 
-faq = json.load(open(faq_json_location, "r"))
-html_template = open(faq_html_template_location, "r").read()
+faq = json.load(open(faq_jsonc_location if use_jsonc else faq_json_location, "r", encoding="utf-8"))
+html_template = open(faq_html_template_location, "r", encoding="utf-8").read()
 text_to_insert = ""
 for entry in faq:
     text_to_insert += faq_html(entry)
 
 html_template = html_template.replace(template_insertion_location, text_to_insert)
 
-open(faq_html_output_location, "w").write(html_template)
+open(faq_html_output_location, "w", encoding="utf-8").write(html_template)
