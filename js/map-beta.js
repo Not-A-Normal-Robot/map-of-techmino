a8aee7f82cf15be39637e125a0fdb9411c68b32f
NOT_A_ROBOT <not_a_normal_robot@protonmail.com>
2024-04-30 05:46:45 +0000

    add html mode shapes

---
+++ js/map-beta.js
@@ -0,0 +1,4 @@
+// DEBUG
+document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+document.getElementsByClassName("square")[0].style.top = "50px";
+document.getElementsByClassName("circle")[0].style.top = "100px";
