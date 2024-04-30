4ec8d8fa4c35da5b64232a1a3ca38164ce73448f
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-04-30 14:48:47 +0700

    rename files, adjust octagon scale

--- js/map-beta.js
+++ js/map-beta.js
@@ -1,4 +1,10 @@
 // DEBUG
 document.getElementsByTagName("body")[0].style.backgroundColor = "black";
-document.getElementsByClassName("square")[0].style.top = "50px";
-document.getElementsByClassName("circle")[0].style.top = "100px";
+let modes = document.getElementsByClassName("mode");
+for(let i = 0; i < modes.length; i++) {
+    const mode = modes[i];
+    mode.style.top = 50 * i + 10;
+    mode.style.left = 10;
+
+    console.log(mode);
+}
