28322c8ded2589f24a8ba8f10ead56de334d59b1
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-02 13:21:37 +0700

    work on html map, add rescaler

--- js/map-beta.js
+++ js/map-beta.js
@@ -1,10 +1,58 @@
-// DEBUG
-document.getElementsByTagName("body")[0].style.backgroundColor = "black";
-let modes = document.getElementsByClassName("mode");
-for(let i = 0; i < modes.length; i++) {
-    const mode = modes[i];
-    mode.style.top = 50 * i + 10;
-    mode.style.left = 10;
+{
+    const root = document.documentElement;
+    const body = document.body;
+    const edgeDisplayCanvas = document.getElementById("edge-display");
+    const edgeDisplayContext = edgeDisplayCanvas.getContext("2d");
 
-    console.log(mode);
+    let camX = 0; let camY = 0; let camZoom = 1;
+
+    let map = {};
+
+    function init() {
+        if(window.self === window.top) {
+            // for easier viewing if directly accessing the page
+            document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+        }
+        loadMapData();
+        onResize();
+    }
+    init();
+
+    function loadMapData(){
+        const urlParams = new URLSearchParams(window.location.search);
+        const mapName = urlParams.get("m") ?? 'vanilla';
+
+        let mapURL = "/data/map/" + mapName.replace(/-/g,"/") + ".json";
+
+        fetch(mapURL)
+            .then(response => {
+                if (!response.ok) throw new Error(`${response.status} - ${response.statusText}`);
+                return response.json();
+            })
+            .then(_map => {
+                map = _map;
+                prevTimestamp = performance.now();
+            })
+            .catch(error => {
+                alert("Error loading map: \n" + error +
+                    "\nPress OK to return to home page.");
+                window.location.href = "/";
+            });
+    }
+
+    function onResize() {
+        const viewportWidth = root.clientWidth || 0;
+        const viewportHeight = root.clientHeight || 0;
+        const viewportDimension = Math.sqrt(viewportWidth * viewportHeight);
+        
+        const targetDimension = Math.sqrt(1280 * 720);
+        const scaleFactor = Math.sqrt(viewportDimension / targetDimension);
+        body.style.setProperty("--scale-factor", scaleFactor);
+    }
+
+    function onMapZoom() {
+
+    }
+
+    window.addEventListener("resize", onResize);
 }
