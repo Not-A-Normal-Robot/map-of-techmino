2c71721a67df680261417133978180ef3fa3f48b
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-04 17:44:33 +0700

    Add edges to HTML map

--- js/map-beta.js
+++ js/map-beta.js
@@ -1,14 +1,13 @@
 "use strict";
 import * as LANG from "./lang.js";
 {
-    
+    const INIT_TIME = performance.now();
     const IS_IN_IFRAME = window.self === window.top;
 
     const ROOT = document.documentElement;
     const BODY = document.body;
     const MAIN_ELEMENT = document.getElementById("main");
-    const EDGE_DSP_CANVAS = document.getElementById("edge-display");
-    const EDGE_DSP_CTX = EDGE_DSP_CANVAS.getContext("2d");
+    const EDGES_SVG = document.getElementById("edge-display");
 
     const MODE_ID_PREFIX = "mode_";
 
@@ -19,7 +18,7 @@ import * as LANG from "./lang.js";
     function init() {
         if(IS_IN_IFRAME) {
             // for easier viewing if directly accessing the page
-            document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+            ROOT.style.backgroundColor = "black";
         }
         loadMapData();
         onResize();
@@ -40,13 +39,20 @@ import * as LANG from "./lang.js";
             })
             .then(_map => {
                 map = _map;
-                addMapToHtml();
             })
             .catch(error => {
-                alert("Error loading map: \n" + error +
-                    "\nPress OK to return to home page.");
+                alert(`Error loading map: \n${error}\n\nPress OK to return to home page.`);
                 window.location.href = "/";
+            })
+            .then(onMapLoad)
+            .catch(error => {
+                alert(`Error after loading map: \n${error}\n\nYou may experience issues if you continue.`);
             });
+    }
+    
+    function onMapLoad() {
+        addMapToHtml();
+        generateEdgeSVG();
     }
 
     function addMapToHtml() {
@@ -81,6 +87,29 @@ import * as LANG from "./lang.js";
             element.style.setProperty("--mode-size", mode.size);
 
             MAIN_ELEMENT.appendChild(element);
+        }
+    }
+
+    function generateEdgeSVG() {
+        EDGES_SVG.setAttribute("viewBox", `${map.min_x} ${map.min_y} ${map.max_x - map.min_x} ${map.max_y - map.min_y}`);
+
+        for(const mode of Object.values(map.modes)) {
+            for(const mode2Name of mode.unlock) {
+                const mode2 = map.modes[mode2Name];
+
+                const x1 = mode.x;  const y1 = mode.y;
+                const x2 = mode2.x; const y2 = mode2.y;
+
+                const edge = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                edge.setAttribute("x1", x1); edge.setAttribute("y1", y1);
+                edge.setAttribute("x2", x2); edge.setAttribute("y2", y2);
+
+                edge.style.setProperty("stroke-width", "calc(4px * var(--scale-factor))");
+                edge.style.setProperty("stroke", "#FFFFFF5F");
+                edge.style.setProperty("stroke-linecap", "round");
+
+                EDGES_SVG.appendChild(edge);
+            }
         }
     }
 
