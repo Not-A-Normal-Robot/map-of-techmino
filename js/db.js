f9a34813e93043804a774de9d23afb0c50fcf1ae
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-16 18:04:46 +0700

    Move some CSS files

--- js/db.js
+++ js/db.js
@@ -1,16 +1,18 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-const SUPABASE_URL = "https://fohgyexhzptaxjqrrrfd.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaGd5ZXhoenB0YXhqcXJycmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMjQxNTcsImV4cCI6MjAyMDYwMDE1N30.fa7XvwiBbWSe2MLIR6Wkh_OC95uV7UXxt7_25PlyAlc"
-const SUPABASE = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const SUPABASE_URL = "https://fohgyexhzptaxjqrrrfd.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaGd5ZXhoenB0YXhqcXJycmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMjQxNTcsImV4cCI6MjAyMDYwMDE1N30.fa7XvwiBbWSe2MLIR6Wkh_OC95uV7UXxt7_25PlyAlc"
+export const SUPABASE = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-console.log(SUPABASE); {
+{
     async function loginWithDiscord() {
         const { data, error } = await SUPABASE.auth.signInWithOAuth({
             provider: "discord",
-
+            options: {
+                redirectTo: "https://techmino-hub.github.io/auth/discord.html"
+            }
         });
         console.log(data, error);
     }
     
-    document.getElementById("button").addEventListener("click", loginWithDiscord);
+    document.getElementById("login")?.addEventListener("click", loginWithDiscord);
 }
