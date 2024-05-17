bae20c4c613e929dbf6b153d79d3c26965cd504c
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-17 15:49:53 +0700

    Try to fix the font loading issue again

--- js/db.js
+++ js/db.js
@@ -3,16 +3,3 @@ export const SUPABASE_URL = "https://fohgyexhzptaxjqrrrfd.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaGd5ZXhoenB0YXhqcXJycmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMjQxNTcsImV4cCI6MjAyMDYwMDE1N30.fa7XvwiBbWSe2MLIR6Wkh_OC95uV7UXxt7_25PlyAlc"
 export const SUPABASE = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-{
-    async function loginWithDiscord() {
-        const { data, error } = await SUPABASE.auth.signInWithOAuth({
-            provider: "discord",
-            options: {
-                redirectTo: "https://techmino-hub.github.io/auth/discord.html"
-            }
-        });
-        console.log(data, error);
-    }
-    
-    document.getElementById("login")?.addEventListener("click", loginWithDiscord);
-}
