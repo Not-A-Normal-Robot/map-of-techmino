b187bfc2e3126971df12c18cf29ab00d75990ffc
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-17 19:52:37 +0700

    make prototype profile setup page

---
+++ js/profile-setup.js
@@ -0,0 +1,52 @@
+{ // Counter for character count
+    for(const counter of document.getElementsByClassName('char-count')) {
+        const associatedId = counter.getAttribute('for');
+        if(!associatedId) {
+            console.error('No associated input found for: ', counter);
+            continue;
+        }
+        
+        const associatedInput = document.getElementById(associatedId);
+        if(!associatedInput) {
+            console.error('No input found with id: ', associatedId);
+            continue;
+        }
+
+        associatedInput.addEventListener('input', () => updateCounter(counter));
+        updateCounter(counter);
+    }
+
+    function updateCounter(element) {
+        const associatedId = element.getAttribute('for');
+        if(!associatedId) {
+            console.error('No associated input found for: ', element);
+            return;
+        }
+        
+        const associatedInput = document.getElementById(associatedId);
+        if(!associatedInput) {
+            console.error('No input found with id: ', associatedId);
+            return;
+        }
+
+        const maxChars = Number(associatedInput.getAttribute('maxlength') ?? Infinity);
+        const currentChars = associatedInput.value.length;
+        element.textContent = `${currentChars} / ${maxChars}`;
+    }
+}
+{ // Handle profile image upload
+    const profileImageInput = document.getElementById('pfp-input');
+    profileImageInput?.addEventListener('change', () => {
+        const file = profileImageInput.files[0];
+        if(!file) return;
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            const image = document.getElementById('pfp-preview');
+            if(image) {
+                image.src = reader.result;
+            }
+        };
+        reader.readAsDataURL(file);
+    });
+}
