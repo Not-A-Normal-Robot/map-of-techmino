a6c0ebaa1ce65eef193c8a7662024dff3b2c2177
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-18 17:54:39 +0700

    Update testing grounds

--- js/db.js
+++ js/db.js
@@ -2,6 +2,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 export const SUPABASE_URL = "https://fohgyexhzptaxjqrrrfd.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaGd5ZXhoenB0YXhqcXJycmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMjQxNTcsImV4cCI6MjAyMDYwMDE1N30.fa7XvwiBbWSe2MLIR6Wkh_OC95uV7UXxt7_25PlyAlc"
 export const SUPABASE = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.SUPABASE = SUPABASE;
 
 /**
  * @typedef {Object} User
