7269e0a39d4e1ab3d9b258eb12e81ac63e171e4e
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-05-18 17:42:57 +0700

    add more debug stuff to testing grounds

--- js/db.js
+++ js/db.js
@@ -3,3 +3,32 @@ export const SUPABASE_URL = "https://fohgyexhzptaxjqrrrfd.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaGd5ZXhoenB0YXhqcXJycmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMjQxNTcsImV4cCI6MjAyMDYwMDE1N30.fa7XvwiBbWSe2MLIR6Wkh_OC95uV7UXxt7_25PlyAlc"
 export const SUPABASE = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+/**
+ * @typedef {Object} User
+ * @property {string} id
+ * @property {string} join_date
+ * @property {string} username
+ * @property {AccountState} account_state
+ */
+
+/**
+ * @typedef {Object} Submission
+ * @property {string} id
+ * @property {string} game_mode
+ * @property {string} username
+ * @property {Object} score
+ * @property {string} upload_date
+ * @property {string} replay_date
+ * @property {string} replay_path
+ * @property {string} submitted_by
+ */
+
+/**
+ * @readonly
+ * @enum {string}
+ */
+export const AccountState = {
+    NORMAL: "normal",
+    BANNED: "banned",
+    UNVERIFIED: "unverified"
+}
